b4c3887855b9305e76ca91be5ea59a650c933485
wudi <390553699@qq.com>
2016-05-16 17:50:09 +0800

    ..

--- app/src/main/java/com/example/wudi/myapplication/MainActivity.java
+++ app/src/main/java/com/example/wudi/myapplication/MainActivity.java
@@ -22,34 +22,27 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void run() {
                 ((TextView)findViewById(R.id.textView)).setText("开机自启动");
-//                Intent Intent = new Intent();
-//                Intent.setClassName("com.touchsprite.android", "com.touchsprite.android.activity.Activity_Login");
-//                startActivity(Intent);
-//                onKeyDown(KeyEvent.KEYCODE_VOLUME_DOWN,null);
-                KeyEvent event = new KeyEvent(System.currentTimeMillis(),KeyEvent.ACTION_DOWN,KeyEvent.KEYCODE_VOLUME_DOWN,0,
-                        0,-1,0,0x0,0x0);
-                onKeyDown(KeyEvent.KEYCODE_BACK,event);
-                Log.i(TAG, "run: "+KeyEvent.KEYCODE_BACK);
+                Intent Intent = new Intent();
+                Intent.setClassName("com.touchsprite.android", "com.touchsprite.android.activity.Activity_Login");
+                startActivity(Intent);
 
+                Intent localIntent = new Intent();
+                localIntent.setClass(MainActivity.this, SpriteIsRunService.class); //销毁时重新启动Service
+                startService(localIntent);
 
-                Log.d(TAG, "reboot");
-                Intent reboot = new Intent(Intent.ACTION_REBOOT);
-                reboot.putExtra("nowait", 1);
-                reboot.putExtra("interval", 1);
-                reboot.putExtra("window", 0);
-                sendBroadcast(reboot);
+//                Log.d(TAG, "reboot");
+//                Intent reboot = new Intent(Intent.ACTION_REBOOT);
+//                reboot.putExtra("nowait", 1);
+//                reboot.putExtra("interval", 1);
+//                reboot.putExtra("window", 0);
+//                sendBroadcast(reboot);
+
             }
         }, 3000);
-
+        finish();
 
     }
 
-    @Override
-    public boolean onKeyDown(int keyCode, KeyEvent event) {
-        Log.i(TAG, "onKeyDown: " + keyCode);
-        Log.i(TAG, "event: " + event);
 
-        return super.onKeyDown(keyCode, event);
-    }
 }
 
